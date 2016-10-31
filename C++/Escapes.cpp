b2381e40236961ff80a3111805b3ec1cd6c31b8a
Vinod <vtkrishn@gmail.com>
2016-10-30 22:24:11 -0700

    changes

---
+++ C++/Escapes.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+
+  std::cout << "Vinod \n \"Krishnan\" \t TK";
+  return 0;
+}
---
+++ C++/getInputFromUser.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+
+  int a, b;
+  std::cout << "getting the input for a";
+  std::cin >> a;
+  std::cout << "getting the input for b";
+  std::cin >> b;
+  std::cout << "The result is " << a + b;
+  return 0;
+}
