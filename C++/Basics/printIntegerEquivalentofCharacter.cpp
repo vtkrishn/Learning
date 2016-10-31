1013bf7b8fd4e146d35dcba8facf0a8e4aba1922
Vinod <vtkrishn@gmail.com>
2016-10-30 23:00:00 -0700

    changes

---
+++ C++/Basics/printIntegerEquivalentofCharacter.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+
+  std::cout << "ASCII equivalent of 'A'" << static_cast <int  >('A') << std::endl;
+  std::cout << "ASCII equivalent of 'b'" << static_cast <int  >('b') << std::endl;
+  std::cout << "ASCII equivalent of '0'" << static_cast <int  >('0') << std::endl;
+  std::cout << "ASCII equivalent of '$'" << static_cast <int  >('$') << std::endl;
+  return 0;
+}
