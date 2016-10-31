e4220e8e582e9847039614f667851002c8994496
Vinod <vtkrishn@gmail.com>
2016-10-30 23:32:40 -0700

    changes

---
+++ C++/Basics/moduloCheck.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main(){
+
+  int a,mod,quot;
+  std::cout << "Enter the value : ";
+  std::cin >> a;
+  mod = a;
+  //print reverse
+  // while(mod != 0){
+  //   quot = mod % 10;
+  //   mod = mod / 10;
+  //   std::cout << quot << "\t";
+  // }
+
+  mod = a;
+  int div = 1;
+  for(int i=mod;i>0;i=i/10){
+    div *= 10;
+    std::cout << div << std::endl;
+  }
+
+  // a = temp % 10;
+  // temp = temp / 10;
+  // std::cout << a << "\t";
+  // a = temp % 10;
+  // temp = temp / 10;
+  // std::cout << a << "\t";
+  return 0;
+}
