f91d45e77515a153a379ea7e2720712d8bcea044
Vinod Thatheri. Krishnan <vtkrishn@oradev.oraclecorp.com>
2016-11-09 01:25:32 -0800

    changes

---
+++ Data Structures/Arrays/StringCompression.java
@@ -0,0 +1,30 @@
+public class YourClassNameHere {
+	    public static void main(String[] args) {
+	     countRepeats("aaaaabbbccde");
+	    }
+
+	    public static void countRepeats(String s1){
+	      if(s1 == null)
+	        return;
+
+	      StringBuilder str = new StringBuilder();
+	      int count=1;
+	      for(int i=1;i<s1.length();i++){
+	        if(s1.charAt(i-1)!=s1.charAt(i)){
+	           str.append(s1.charAt(i-1));
+	           str.append(count);
+	           count=1;
+	        }
+	        else{
+	          count++;
+	        }
+	      }
+
+	      if(s1.charAt(s1.length()-1) != s1.charAt(s1.length()-2)){
+	        str.append(s1.charAt(s1.length()-1));
+	        str.append(1);
+	      }
+
+	      System.out.println(str.toString());
+	    }
+	}
