0bdf7c44788b161b97990b896f9278dfe161107b
Vinod Thatheri. Krishnan <vtkrishn@oradev.oraclecorp.com>
2016-11-12 21:00:04 -0800

    changes

---
+++ Data Structures/Arrays/moveArrayToEnd.java
@@ -0,0 +1,19 @@
+public class YourClassNameHere {
+	    public static void main(String[] args) {
+	      int[] mn = {2,3,0,9,14,0,0,0,21};
+	      moveToEnd(mn);
+	    }
+
+	    public static void moveToEnd(int[] mn)
+      {
+        int j = mn.length - 1;
+        for (int i = mn.length - 1; i >= 0; i--)
+        {
+            if (mn[i] != 0)
+            {
+                mn[j] = mn[i];
+                j--;
+            }
+        }
+    }
+  }
