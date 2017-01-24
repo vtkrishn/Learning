653928cad51ff7bbab8bac49df549f95d853cb6f
Vinod Thatheri. Krishnan <vtkrishn@oradev.oraclecorp.com>
2017-01-23 21:07:09 -0800

    changes

--- Practice/LeetCode/Easy/HammingDistance.java
+++ Practice/LeetCode/Easy/HammingDistance.java
@@ -11,3 +11,34 @@ public class Solution {
         return count;
     }
 }
+
+------
+
+public class Solution {
+    public int hammingDistance(int x, int y) {
+        int m = x ^ y;
+        int count = 0;
+        while(m != 0){
+            m = m & (m-1);
+            count++;
+        }
+        return count;
+    }
+}
+
+-------
+
+http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/6-b14/java/lang/Integer.java#Integer.bitCount%28int%29
+
+public class Solution {
+    public int hammingDistance(int x, int y) {
+        int i = x ^ y;
+        i = i - ((i >>> 1) & 0x55555555);
+        i = (i & 0x33333333) + ((i >>> 2) & 0x33333333);
+        i = (i + (i >>> 4)) & 0x0f0f0f0f;
+        i = i + (i >>> 8);
+        i = i + (i >>> 16);
+        return i & 0x3f;
+        //return Integer.bitCount(x ^ y);
+    }
+}
