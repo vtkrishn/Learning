720376a5381fbaa408b99e2c08d3f2395923fef3
Vinod Thatheri. Krishnan <vtkrishn@oradev.oraclecorp.com>
2017-02-20 20:02:19 -0800

    'changes'

--- Languages/HTML5/script.js
+++ Languages/HTML5/script.js
@@ -1,17 +1,34 @@
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
+var slider = document.getElementById("slider");
 
 var draw = function() {
   var length = document.getElementById("length").value;
   var depth = document.getElementById("depth").value;
   canvas.width = length * 2;
   canvas.height = length * 2;
+  context.clearRect(0, 0, canvas.width, canvas.height);
   var x = canvas.width/2;
-  var y = canvas.height/2;
-
-  drawCircle(x,y,length/50);
+  var y = canvas.height;
+  var value = 0.1 * slider.value;
+  drawBranch(x,y,100,depth);
 }
 
+var drawBranch = function(x,y,length, depth) {
+
+  if(depth == 0)
+    return;
+
+  drawLine(x,y,x,y-length);
+  //left
+  drawLine(x,y-length,x-length/depth,y-length*value);
+  //right
+
+  drawLine(x,y-length,x+length/depth,y-length*value);
+
+  drawBranch(x-length/depth,y-length*value,length/2,depth-1);
+  drawBranch(x+length/depth,y-length*value,length/2,depth-1);
+}
 
 
 var drawLine = function(x1,y1,x2,y2) {
