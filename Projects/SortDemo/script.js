d86609f87d7f4804b6a0aa27c6ae98f9ca933946
Vinod Thatheri. Krishnan <vtkrishn@oradev.oraclecorp.com>
2017-02-27 00:37:41 -0800

    'changes'

--- Projects/SortDemo/script.js
+++ Projects/SortDemo/script.js
@@ -1,26 +1,27 @@
 var arr = document.getElementById('array');
 var count = 0;
-var max = 200;
+var max = 100;
 
 for(var i = 0;i<max;i++){
 	var c = document.createElement('div');
 	c.id = 'child' + (count++);
-	c.style.cssText = 'width : ' + parseInt(Math.random()* (100 - 10) + 10) + 'px; height:'+ .5 +'px; border: 1px solid;transition: 25s;';
+	c.style.cssText = 'width : ' + parseInt(Math.random()* (100 - 10) + 10) + 'px; height:'+ 1 +'px; border: 1px solid;transition: .5s;';
 	arr.appendChild(c);
 }
 
-var sort = function (){
-	for(var i=0;i<max;i++){
-		for(var j=0;j<max;j++){
-			
+function getChildWidth(child){
+	var childWidth = child.style.width.length;
+	return child.style.width.substring(0,childWidth-2);
+}
+
+var bubbleSort = function (){
+	for(var i=max-1;i>=0;i--){
+		for(var j=0;j<=i;j++){
 			var child0 = document.getElementById('child'+i);
 			var child1 = document.getElementById('child'+j);
 			
-			var child0Width = child0.style.width.length;
-			var child1Width = child1.style.width.length;
-			
-			var a = child0.style.width.substring(0,child0Width-2);
-			var b = child1.style.width.substring(0,child1Width-2);
+			var a = getChildWidth(child0);
+			var b = getChildWidth(child1);
 				if(b > a){
 						child0.style.width = b+'px';
 						child1.style.width = a+'px';
@@ -28,4 +29,46 @@ var sort = function (){
 		}
 	}
 
+}
+
+var selectionSort = function (){
+	for(var i=0;i<max;i++){
+		var child0 = document.getElementById('child'+i);
+		var a = getChildWidth(child0);
+		var min = i;
+		for(var j=i;j<max;j++){
+			var child1 = document.getElementById('child'+j);
+			var b = getChildWidth(child1);
+			
+			var childMin = document.getElementById('child'+min);
+			var c = getChildWidth(childMin);			
+
+			if(b < c){
+					min = j;
+				}
+		}
+		child0.style.width = c+'px';
+		childMin.style.width = a+'px';
+	}
+
+}
+
+var insertionSort = function (){
+  for(var i = 1; i < max; i++) {
+	var childi = document.getElementById('child'+i);
+	var a = getChildWidth(childi);
+	
+	var j = i - 1;
+	var childj = document.getElementById('child'+j);
+	var b = getChildWidth(childj);
+	
+	var k = j + 1;
+	var childk = document.getElementById('child'+k);
+	var c = getChildWidth(childk);
+	
+    for(; j >= 0 && b > a; j--) {
+		childk.style.width = b+'px';
+    }
+		childk.style.width = a+'px';
+  }
 }
