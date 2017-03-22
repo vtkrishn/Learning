662ef7512ae179d34c32c065fd77b9f2aad68171
Vinod Thatheri. Krishnan <vtkrishn@oradev.oraclecorp.com>
2017-03-21 19:50:27 -0700

    changes

---
+++ Practice/sample/Reservation.java
@@ -0,0 +1,15 @@
+package openTable;
+
+import java.time.LocalTime;
+
+public class Reservation {
+    public String name;
+    public int partySize;
+    public  LocalTime startTime;
+
+    public Reservation(String name, int partySize, LocalTime  startTime){
+        this.name = name;
+        this.partySize = partySize;
+        this.startTime = startTime;
+    }
+}
