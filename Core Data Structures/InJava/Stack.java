db2ed0a2a0f9144e53b0e161be78de3517ebb2e0
Vinod Thatheri. Krishnan <vtkrishn@oradev.oraclecorp.com>
2017-03-23 23:11:58 -0700

    changes

--- Core Data Structures/InJava/Stack.java
+++ Core Data Structures/InJava/Stack.java
@@ -1,3 +1,8 @@
+package javaproject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyStack{
   List<Integer> array;
   public MyStack(){
@@ -8,7 +13,36 @@ public class MyStack{
     return this.array.size() == 0;
   }
 
-  public
+  public int pop(){
+    if(isEmpty())
+      System.out.println("Nothing to pop");
+    else{
+        int size = this.array.size();
+        int value = this.array.remove(size-1);
+        System.out.println("popped "+ value);
+        return value;
+    }
+    return -1;
+  }
+
+  public int peek(){
+    if(isEmpty())
+      System.out.println("Nothing to peek");
+      else{
+          int size = this.array.size();
+          int value = this.array.get(size-1);
+          System.out.println("peeked "+ value);
+          return value;
+      }
+      return -1;
+  }
+
+  public void push(int value){
+    this.array.add(value);
+      System.out.println("pushed "+ value);
+  }
+
+
 
   public static void main(String[] args){
     MyStack stack = new MyStack();
@@ -16,14 +50,17 @@ public class MyStack{
     stack.push(2);
     stack.push(3);
     stack.peek();
-    stack.push(1);
+    stack.push(13);
     stack.peek();
 
     stack.pop();
     stack.peek();
     stack.pop();
     stack.peek();
-    
+      stack.pop();
+      stack.pop();
+      stack.pop();
+      stack.peek();
   }
 
 }
