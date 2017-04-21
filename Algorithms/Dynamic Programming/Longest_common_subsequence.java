df5ebede259c9e1ad092cf097723d54822d27897
Vinod Krishnan <vin.sinin@gmail.com>
2017-04-21 00:48:19 -0700

    'changes'

--- Algorithms/Dynamic Programming/Longest_common_subsequence.java
+++ Algorithms/Dynamic Programming/Longest_common_subsequence.java
@@ -1,3 +1,27 @@
+package javaproject;
+
+public class LeastCommonSubsequence {
+    public LeastCommonSubsequence() {
+        super();
+    }
+
+    public static void main(String[] args){
+        String s1 = "BA";
+        String s2 = "BBAB";
+        System.out.println(LCS(s1, s2, s1.length(), s2.length()));
+    }
+
+    private static int LCS(String s1,String s2,int i, int j){
+        if(i == 0 || j == 0)
+            return 0;
+        if(s1.charAt(i-1) == s2.charAt(j-1))
+            return LCS(s1, s2, i-1, j-1) + 1;
+        else
+            return Math.max(LCS(s1,s2,i-1,j),LCS(s1,s2,i,j-1));
+    }
+}
+
+------------------------------------
 public class YourClassNameHere {
     public static void main(String[] args) {
         String s1 = "AGGTAB";//"ABCDH";
---
+++ Algorithms/Dynamic Programming/PathFinding.java
@@ -0,0 +1,29 @@
+package javaproject.dynamic;
+
+public class PathFinding {
+    public PathFinding() {
+        super();
+    }
+
+    public static void main(String[] args){
+        int[][] arr = {
+                    {1,0,0},
+                    {1,0,0},
+                    {1,1,1}
+                    };
+        int row = arr.length-1;
+        int col = arr[0].length-1;
+        boolean flag = pathExist(arr, row, col,0,0);
+        System.out.println(flag);
+    }
+
+    public static boolean pathExist(int[][] arr, int row, int col, int i , int j){
+        if(i > row || j > col || arr[i][j] == 0)
+            return false;
+        if(i == row && j == col)
+            return true;
+        //cache the i,j and optimize it
+        return pathExist(arr, row, col, i+1, j) || pathExist(arr, row, col, i, j+1);
+    }
+
+}
