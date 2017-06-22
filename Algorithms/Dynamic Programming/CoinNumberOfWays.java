6d2d96d2a6b80e211544c4f773dbae4c53a77989
Vinod Krishnan <vin.sinin@gmail.com>
2017-06-22 00:37:43 -0700

    'changes'

--- Algorithms/Dynamic Programming/CoinNumberOfWays.java
+++ Algorithms/Dynamic Programming/CoinNumberOfWays.java
@@ -1,3 +1,28 @@
+import java.util.Arrays;
+public class CoinsRecursion {
+    public static void main(String[] args) {
+        //int[] coins = {2,3,7};
+        int[] coins = {3,5,10};
+        System.out.println(makeChanges(coins, 13, 0));
+    }
+
+        public static int makeChanges(int[] coins,int money, int index){
+          if(money == 0)
+            return 1;
+          if(index == coins.length)
+            return 0;
+          int amount = 0;
+          int ways = 0;
+          while(amount <= money){
+            int remaining = money - amount;
+            ways += makeChanges(coins, remaining, index + 1);
+            amount+= coins[index];
+          }
+          return ways;
+        }
+}
+----------------------------------
+
 package javaproject;
 
 import java.util.Arrays;
