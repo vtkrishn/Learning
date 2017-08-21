d3839d5b40cad09f9f40676009fcadea07c6bad4
Vinod Krishnan <vin.sinin@gmail.com>
2017-08-20 23:21:43 -0700

    'changes'

---
+++ Practice/LeetCode/Hard/Remove9.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int newInteger(int n) {
+        int ans = 0;
+	    int base = 1;
+
+        while (n > 0){
+            ans += n % 9 * base;
+            n /= 9;
+            base *= 10;
+        }
+	    return ans;
+    }
+}
