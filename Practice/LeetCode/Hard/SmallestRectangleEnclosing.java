831b59beccd2ee23a1ceba46b34e3342f5a48d5e
Vinod Krishnan <vin.sinin@gmail.com>
2017-08-23 00:07:59 -0700

    'changes'

--- Practice/LeetCode/Hard/SmallestRectangleEnclosing.java
+++ Practice/LeetCode/Hard/SmallestRectangleEnclosing.java
@@ -6,11 +6,11 @@ public class ScratchPad {
     static int bottom;
     static int left;
     static int right;
-    
+
     public ScratchPad() {
         super();
     }
-    
+
     public static void main(String[] args){
         int[][] image  = {
                           {0,0,1,0},
@@ -25,26 +25,26 @@ public class ScratchPad {
         findSolution(image,0,2,visited);
         System.out.println(area);
     }
-    
+
     public static void findSolution(int[][] image,int x, int y,boolean[][] visited){
         if(x < 0 || x >= image.length || y < 0 || y >= image[0].length || image[x][y] == 0 || visited[x][y] == true)
             return;
-        
+
         visited[x][y] = true;
-        
+
         top = Math.min(top,y);
         bottom = Math.max(bottom, y);
         left = Math.min(left,x);
         right = Math.max(right, x);
-        
+
         int currArea = (right-left + 1) * (bottom - top + 1);
         area = Math.max(area,currArea);
-        
+
         findSolution(image,x,y-1,visited);
         findSolution(image,x,y+1,visited);
         findSolution(image,x-1,y,visited);
         findSolution(image,x+1,y,visited);
-        
+
     }
-    
+
 }
