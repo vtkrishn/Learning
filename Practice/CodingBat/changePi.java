b250669a71502ccfb8f87d2622868cded1c2c84a
Vinod Krishnan <vin.sinin@gmail.com>
2017-09-09 20:27:55 -0700

    'changes'

---
+++ Practice/CodingBat/changePi.java
@@ -0,0 +1,7 @@
+public String changePi(String str) {
+  if (str.equals("") || str.length() < 2)
+    return str;
+   if(str.substring(0,2).equals("pi"))
+     return "3.14" + changePi(str.substring(2));
+  return str.charAt(0) + changePi(str.substring(1));
+}
