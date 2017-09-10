b0015290793b896ec1a9adb281417ab2655690ba
Vinod Krishnan <vin.sinin@gmail.com>
2017-09-10 02:00:30 -0700

    'changes'

--- Practice/Jdeveloper/Trial/com/learning/scratchpad/WordLadder.java
+++ Practice/Jdeveloper/Trial/com/learning/scratchpad/WordLadder.java
@@ -2,7 +2,9 @@ package com.learning.scratchpad;
 
 import java.util.*;
 public class WordLadder {
-    static Set<String> processedSet = new HashSet<String>();
+    static List<List<String>> result = new ArrayList<List<String>>();
+    static List<String> tempList = new ArrayList<String>();
+    static Map<String,List<String>> map = new HashMap<String,List<String>>();
     
     public static void main(String[] args){
       List<String> wordList = new ArrayList<String>();
@@ -16,41 +18,45 @@ public class WordLadder {
       findLadders("hit","cog",wordList);
     }
     public static List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-        List<List<String>> result = new ArrayList<List<String>>();
-        List<String> tempList = new ArrayList<String>();
+        Set<String> visited= new HashSet<String>();
+        Set<String> unVisited= new HashSet<String>(wordList);
+        
         LinkedList<String> queue = new LinkedList<String>();
         queue.offer(beginWord);
+        
+        unVisited.add(endWord);
+        unVisited.remove(beginWord);
+        
         while(!queue.isEmpty()){
             String word = queue.poll();
             if(!tempList.contains(word))
                 tempList.add(word);
+            
             if(word.equals(endWord)){
                 result.add(tempList);
             }
             else{
-             String output = checkWord(word, wordList);
-                if(output != null){
-                    queue.offer(output);
-                }   
+                for(int j = 0;j<beginWord.length();j++){
+                    char[] tempChar = beginWord.toCharArray();
+                    //permute
+                    for(char i='a';i<='z';i++){
+                        tempChar[j] = i;
+                        String tempWord = new String(tempChar);
+                        //check if it exists in the wordList
+                        if(wordList.contains(tempWord) && !visited.contains(tempWord)){
+                            visited.add(tempWord);
+                            queue.offer(tempWord);
+                        }
+                        
+                        if(map.containsKey(tempWord))
+                            map.get(tempWord).add(word);
+                        else{
+                            
+                        }
+                    }      
+                }
             }
         }
         return result;
-    }
-    
-    private static String checkWord(String beginWord, List<String> wordList){
-        for(int j = 0;j<beginWord.length();j++){
-            char[] tempChar = beginWord.toCharArray();
-            //permute
-            for(char i='a';i<='z';i++){
-                tempChar[j] = i;
-                String tempWord = new String(tempChar);
-                //check if it exists in the wordList
-                if(wordList.contains(tempWord) && !processedSet.contains(tempWord)){
-                    processedSet.add(tempWord);
-                    return tempWord;
-                }
-            }      
-        }
-        return null;
     }
 }
