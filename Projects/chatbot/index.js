f155b0d4f808b201140ae68110a6993d2cf74a4a
Vinod Krishnan <vin.sinin@gmail.com>
2017-10-11 23:51:56 -0700

    changes

--- Projects/chatbot/index.js
+++ Projects/chatbot/index.js
@@ -1,24 +1,24 @@
 'use strict';
 
 var express = require('express');
-var body = require('body-parser');
+var bodyParser = require('body-parser');
 var request = require('request');
 
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(body.urlenconder({extended: false}));
-app.use(body.json());
+app.use(bodyParser.urlenconded({extended: false}));
+app.use(bodyParser.json());
 
 
 app.get('/', function(req,res){
-    res.send("Hi I am chatbot");
+    res.send("Hi I am a chatbot");
 });
 
 app.get('/webhook/',function(req,res){
     if(req.query['hub.verify_token'] === "vtkrishn")
-        res.send(req.query('hub.challenge'));
+        res.send(req.query['hub.challenge']);
     res.send("Wrong Token");
 });
 
